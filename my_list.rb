e6aa61c9687e035651643e9708e8026db3e888cb
Roshan Bajgain <roshan.bazgain@gmail.com>
2022-04-01 14:45:56 +0545

    Puts list all,any and filter to cheek the condition

--- my_list.rb
+++ my_list.rb
@@ -7,10 +7,19 @@ class MyList
   def initialize(*list)
     @list = list
   end
-  
+
   def each
     @list.each { |x| yield x }
   end
 
 end
 
+list = MyList.new(1, 2, 3, 4)
+
+puts list.all? { |e| e < 5}
+puts list.all? { |e| e > 5}
+
+puts list.any? {|e| e == 2}
+puts list.any? {|e| e == 5}
+
+puts list.filter { |e| e.even? }
